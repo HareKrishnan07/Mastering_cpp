a1bbcb49703d7fe23d27b58fb37a538dda15e175
HareKrishnan07 <harekrishnan14007@gmail.com>
2025-05-20 23:07:47 +0530

    Day 1:Adding two num

---
+++ 2.Adding two num/two num with function.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int addnumber( int a , int b)
+{
+    int result = a+b;
+    return result;
+    }
+
+int main()
+{
+    int firstnum;
+    int secondnum;
+
+    cout<<"Enter The First Number:";
+    cin>>firstnum;
+    cout<<"Enter The Second Number:";
+    cin>>secondnum;
+    int sum = addnumber(firstnum,secondnum);
+    cout<<"Sum of both number is "<<sum<<endl;
+
+    return 0;
+
+
+}
---
+++ 2.Adding two num/two.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int firstnum;
+    int secondnum;
+    cout<<"Enter The First Number:";
+    cin>>firstnum;
+    cout<<"Enter The Second Number:";
+    cin>>secondnum;
+    cout<<"Sum of both number is "<<firstnum+secondnum;    
+}
